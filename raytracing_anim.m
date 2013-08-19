d8c2e26647a30792d7ba17015f27d66e0c506bab
Teivaz <o.kuznietsov@gmail.com>
2013-08-19 17:00:42 +0300

    Added customizable walls and obstacles set

--- raytracing_anim.m
+++ raytracing_anim.m
@@ -13,7 +13,7 @@ L7 = [+0.0, +1.0, +0.0];    % [A, B, C] -> Ax + By + C = 0
 L8 = [+0.0, +1.0, +5.0];    % [A, B, C] -> Ax + By + C = 0
 
 %walls = [L1; L2; L3; L4; L5; L6];
-walls = [L3; L6; L1; L5];
+walls = [0];
 %floor = [L7; L8];
 floor = [L7];
 
@@ -31,9 +31,9 @@ for fooo = 1:n
     y2_ = 3.4;
     x2_ = 6.3;
     x1 = x1_ + ds(fooo);
-    x2 = x2_ ;%+ ds(fooo);
+    x2 = x2_ - ds(fooo);
     y1 = y1_ + ds(fooo);
-    y2 = y2_ ;%+ ds(fooo);
+    y2 = y2_ + ds(fooo);
 
 
 
@@ -56,55 +56,58 @@ aSq = 1; % len(1)^2;
 ind = 1;
 len = zeros(1, 9);
 len(ind) = norm(Rx - Tx);
-for L = walls'
-    TxP = ProjectionPoint(Tx(1:2), L);
-    RxP = ProjectionPoint(Rx(1:2), L);
-    k = norm(RxP - Rx(1:2))/norm(TxP - Tx(1:2));
-    l = norm(TxP - RxP);
-    direction = (RxP - TxP)/l;
-    %Reflection Point
-    Rp = TxP + direction*(l/(k+1));
-    path = norm(Tx(1:2) - Rp) + norm(Rp - Rx(1:2));
-    ind = ind+1;
-    len(ind) = path;
-    [X, Y] = ToParamteric(L, p);
-    %Angle
-    b = norm(Tx(1:2) - Rp);
-    c = norm(Rp - Rx(1:2));
-    indc = indc+1;
-    cosAngle(indc) = -(aSq - b^2 - c^2)/(4*b*c);
-    
-    plot(X, Y, 'k');
-    scatter(Rp(1), Rp(2), 'r', 'filled');
-    scatter(RxP(1), RxP(2), 'b', 'filled');
-    scatter(TxP(1), TxP(2), 'b', 'filled');
-    
-end
-for L = floor'
-    tx = [0.0, Tx(3)];
-    los = sqrt(aSq);
-    rx = [los, Rx(3)];
-    
-    TxP = ProjectionPoint(tx, L);
-    RxP = ProjectionPoint(rx, L);
-    k = norm(RxP - rx)/norm(TxP - tx);
-    direction = (RxP - TxP)/len(1);
-    %Reflection Point
-    Rp = TxP + direction*(len(1)/(k+1));
-    path = norm(tx - Rp) + norm(Rp - rx);
-    ind = ind+1;
-    len(ind) = path;
-    %[X, Y] = ToParamteric(L, p);
-    %Angle
-    b = norm(Tx(1:2) - Rp);
-    c = norm(Rp - Rx(1:2));
-    indc = indc+1;
-    cosAngle(indc) = -(aSq - b^2 - c^2)/(4*b*c);
-    
-%    plot(X, Y, '--b');
-%    scatter(Rp(1), Rp(2), 'r', 'filled');
-end
+if size(walls, 2) > 1
+    for L = walls'
+        TxP = ProjectionPoint(Tx(1:2), L);
+        RxP = ProjectionPoint(Rx(1:2), L);
+        k = norm(RxP - Rx(1:2))/norm(TxP - Tx(1:2));
+        l = norm(TxP - RxP);
+        direction = (RxP - TxP)/l;
+        %Reflection Point
+        Rp = TxP + direction*(l/(k+1));
+        path = norm(Tx(1:2) - Rp) + norm(Rp - Rx(1:2));
+        ind = ind+1;
+        len(ind) = path;
+        [X, Y] = ToParamteric(L, p);
+        %Angle
+        b = norm(Tx(1:2) - Rp);
+        c = norm(Rp - Rx(1:2));
+        indc = indc+1;
+        cosAngle(indc) = -(aSq - b^2 - c^2)/(4*b*c);
 
+        plot(X, Y, 'k');
+        scatter(Rp(1), Rp(2), 'r', 'filled');
+        scatter(RxP(1), RxP(2), 'b', 'filled');
+        scatter(TxP(1), TxP(2), 'b', 'filled');
+
+    end
+end
+if size(floor, 2) > 1
+    for L = floor'
+        tx = [0.0, Tx(3)];
+        los = sqrt(aSq);
+        rx = [los, Rx(3)];
+
+        TxP = ProjectionPoint(tx, L);
+        RxP = ProjectionPoint(rx, L);
+        k = norm(RxP - rx)/norm(TxP - tx);
+        direction = (RxP - TxP)/len(1);
+        %Reflection Point
+        Rp = TxP + direction*(len(1)/(k+1));
+        path = norm(tx - Rp) + norm(Rp - rx);
+        ind = ind+1;
+        len(ind) = path;
+        %[X, Y] = ToParamteric(L, p);
+        %Angle
+        b = norm(Tx(1:2) - Rp);
+        c = norm(Rp - Rx(1:2));
+        indc = indc+1;
+        cosAngle(indc) = -(aSq - b^2 - c^2)/(4*b*c);
+
+    %    plot(X, Y, '--b');
+    %    scatter(Rp(1), Rp(2), 'r', 'filled');
+    end
+end
 %hold off;
 xlim([-0.1, 7.1]);
 ylim([-0.1, 6.1]);
@@ -134,6 +137,18 @@ ex1_time = h14(:,1);
 ex1_ampl = h14(:,2);
 ex1_ampl = ex1_ampl./max(ex1_ampl);
 plot(ex1_time, ex1_ampl, 'kx');
+% ex1_time = h13(:,1);
+% ex1_ampl = h13(:,2);
+% ex1_ampl = ex1_ampl./max(ex1_ampl);
+% plot(ex1_time, ex1_ampl, 'xg');
+% ex1_time = h24(:,1);
+% ex1_ampl = h24(:,2);
+% ex1_ampl = ex1_ampl./max(ex1_ampl);
+% plot(ex1_time, ex1_ampl, 'xb');
+% ex1_time = h23(:,1);
+% ex1_ampl = h23(:,2);
+% ex1_ampl = ex1_ampl./max(ex1_ampl);
+% plot(ex1_time, ex1_ampl, 'xk');
 hold off;
 
 drawnow;
