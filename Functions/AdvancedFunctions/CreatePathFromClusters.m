9dad5d77d2755489eec0ced8f79dfe1fadf27449
Teivaz <o.kuznietsov@gmail.com>
2013-10-12 15:30:45 +0300

    Moved generating clusters to function CreatePathFromClusters.m

---
+++ Functions/AdvancedFunctions/CreatePathFromClusters.m
@@ -0,0 +1,34 @@
+function [Y, impResp, two] = CreatePathFromClusters(clusters, time)
+%CREATEPATHFROMCLUSTERS Summary of this function goes here
+%   Detailed explanation goes here
+
+NumberOfClusters = numel(clusters);
+
+impResp = [];
+figure(2);
+for a = 1:NumberOfClusters
+    [x, y] = create_cluster2(clusters(a));
+    impResp(a).x = x;
+    impResp(a).y = y;
+end
+
+two = [];
+for a = 1:NumberOfClusters
+    two(a).x = time;
+    two(a).y = resample_array(impResp(a).x, impResp(a).y, time);
+    Fs = 1/(time(2) - time(1));
+    d = fdesign.lowpass('n,fp,fst,ast', 17, 2400e6, 2480e6, 100, Fs);
+    hd = design(d);
+    two(a).y = filter(hd, two(a).y);
+    two(a).y = filter(hd, two(a).y);
+    two(a).y = filter(hd, two(a).y);
+    two(a).y = filter(hd, two(a).y);
+    two(a).y = filter(hd, two(a).y);
+end
+Y = two(1).y;
+for a = 2:NumberOfClusters
+    Y = two(a).y + Y;
+end
+
+end
+
--- find clusters/GenerateClusters.m
+++ find clusters/GenerateClusters.m
@@ -8,13 +8,13 @@ CImpulseResponse.x = [];
 CImpulseResponse.y = [];
 
 % Limits
-ClusterLimits.StartTime = [6e-9, 30e-9];     % s
+ClusterLimits.StartTime = [0e-9, 30e-9];     % s
 ClusterLimits.IntervalTime = [1e-9, 20e-9];  % s
 ClusterLimits.StartPower = [0.01, 1];        % V
-ClusterLimits.DecayPower = [0.9, 10];          % 
-ClusterLimits.Number = [2, 8];              % 
+ClusterLimits.DecayPower = [0.9, 10];        % 
+ClusterLimits.Number = [2, 5];               % 
 
-NumberOfClusters = 3;
+NumberOfClusters = 4;
 
 rnd = @(a) a(1) + (a(2)-a(1)).*rand(1,1);
 rndi = @(a) a(1) + randi((a(2)-a(1)),1,1);
@@ -27,14 +27,15 @@ for a = 1:NumberOfClusters
     clusters(a).DecayPower   = rnd(ClusterLimits.DecayPower);   
     clusters(a).Number       = rndi(ClusterLimits.Number);    
 end
-
+%
 colors = hsv(NumberOfClusters);
-impResp = [];
-figure(2);
+time = 1:0.1:140;
+time = time.*1e-9;
+
+[Y, impResp, two] = CreatePathFromClusters(clusters, time);
+
+%figure(2);
 for a = 1:NumberOfClusters
-    [x, y] = create_cluster2(clusters(a));
-    impResp(a).x = x;
-    impResp(a).y = y;
     h = stem(impResp(a).x, impResp(a).y);
     set(h, 'color', colors(a,:));
     set(get(h,'BaseLine'),'BaseValue',1e-6);
@@ -44,33 +45,15 @@ end
 %hold off;
 %figure(1)
 
-time = 1:0.1:140;
-time = time.*1e-9;
-
-two = [];
 for a = 1:NumberOfClusters
-    two(a).y = resample_array(impResp(a).x, impResp(a).y, time);
-    Fs = 1/(time(2) - time(1));
-    d = fdesign.lowpass('n,fp,fst,ast', 5, 2500e6, 2800e6, 30, Fs);
-    hd = design(d);
-    two(a).y = filter(hd, two(a).y);
-    two(a).y = filter(hd, two(a).y);
-    two(a).y = filter(hd, two(a).y);
-    two(a).y = filter(hd, two(a).y);
-    two(a).y = filter(hd, two(a).y);
-
-    %h = semilogy(time, abs(two(a).y));
-    %set(h, 'color', colors(a,:));
+    h = semilogy(time, abs(two(a).y));
+    set(h, 'color', colors(a,:));
     hold on;
 end
+semilogy(time, Y, 'k:');
+ylim([1e-6, 1]);
 hold off;
 
-Y = two(1).y;
-for a = 2:NumberOfClusters
-    Y = two(a).y + Y;
-end
-semilogy(time, Y);
-ylim([1e-6, 1]);
 
 %%
 clear plt;
